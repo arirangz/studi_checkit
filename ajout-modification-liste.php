b9f829efd3b32738696b1ff6d5bfaeecf0287d72
Anthony Laplane <anthony.laplane@gmail.com>
2024-04-24 18:58:32 +0200

    live 6

--- ajout-modification-liste.php
+++ ajout-modification-liste.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-require_once __DIR__ . "/templates/header.php"; 
+require_once __DIR__ . "/templates/header.php";
 require_once 'lib/pdo.php';
 require_once 'lib/list.php';
 require_once 'lib/category.php';
@@ -12,13 +12,28 @@ if (!isUserConnected()) {
 $categories = getCategories($pdo);
 
 $errorsList = [];
+$errorsListItem = [];
+$messagesList = [];
+
+$list = [
+    'title' => '',
+    'category_id' => ''
+];
 
 // Le formulaire d'ajout/modif de liste a été envoyé
 if (isset($_POST['saveList'])) {
     if (!empty($_POST['title'])) {
-        $res = saveList($pdo, $_POST['title'], (int)$_SESSION['user']['id'], $_POST['category_id']);
+        $id = null;
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+        }
+        $res = saveList($pdo, $_POST['title'], (int)$_SESSION['user']['id'], $_POST['category_id'], $id);
         if ($res) {
-            header('Location: ajout-modification-liste.php?id=' . $res);
+            if ($id) {
+                $messagesList[] = 'La liste a bien été mise à jour';
+            } else {
+                header('Location: ajout-modification-liste.php?id=' . $res);
+            }
         } else {
             // erreur
             $errorsList[] = "La liste n'a pas été enregistrée";
@@ -28,7 +43,22 @@ if (isset($_POST['saveList'])) {
         $errorsList[] = "Le titre est obligatoire";
     }
 }
-  
+if (isset($_POST['saveItem'])) {
+    if (!empty($_POST['name'])) {
+        // sauvegarder
+        $res = saveListItem($pdo, $_POST['name'], (int)$_GET['id'], false);
+    } else {
+        // erreur
+        $errorsListItem[] = "Le nom de l'item est obligatoire";
+    }
+}
+
+$editMode = false;
+if (isset($_GET['id'])) {
+    $list = getListById($pdo, (int)$_GET['id']);
+    $editMode = true;
+}
+
 
 
 ?>
@@ -38,32 +68,33 @@ if (isset($_POST['saveList'])) {
 
     <?php foreach ($errorsList as $error) { ?>
         <div class="alert alert-danger">
-            <?=$error; ?>
+            <?= $error; ?>
+        </div>
+    <?php } ?>
+    <?php foreach ($messagesList as $message) { ?>
+        <div class="alert alert-success">
+            <?= $message; ?>
         </div>
     <?php } ?>
     <div class="accordion" id="accordionExample">
         <div class="accordion-item">
             <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <?php if (isset($_GET['id'])) { ?>
-                        Modifier la liste
-                    <?php } else { ?>
-                        Ajouter une liste
-                    <?php } ?>
+                <button class="accordion-button <?= ($editMode) ? 'collapsed' : '' ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="<?= ($editMode) ? 'false' : 'true' ?>" aria-controls="collapseOne">
+                    <?= ($editMode) ? $list['title'] : 'Ajouter une liste' ?>
                 </button>
             </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div id="collapseOne" class="accordion-collapse collapse <?= ($editMode) ? '' : 'show' ?>" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                     <form action="" method="post">
                         <div class="mb-3">
                             <label for="title" class="form-label">Titre</label>
-                            <input type="text" name="title" id="title" class="form-control">
+                            <input type="text" value="<?= $list['title']; ?>" name="title" id="title" class="form-control">
                         </div>
                         <div class="mb-3">
                             <label for="category_id" class="form-label">Catégorie</label>
                             <select name="category_id" id="category_id" class="form-control">
                                 <?php foreach ($categories as $category) { ?>
-                                    <option value="<?=$category['id'] ?>"><?=$category['name'] ?></option>
+                                    <option <?= ($category['id'] === $list['category_id']) ? 'selected="selected"' : '' ?> value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
                                 <?php } ?>
                             </select>
                         </div>
@@ -76,6 +107,25 @@ if (isset($_POST['saveList'])) {
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row mt-3">
+        <?php if (!$editMode) { ?>
+            <div class="alert alert-warning">
+                Après avoir enregistré, vous pourrez ajouter les items.
+            </div>
+        <?php } else { ?>
+            <h2 class="border-top pt-3">Items</h2>
+            <?php foreach ($errorsListItem as $error) { ?>
+                <div class="alert alert-danger">
+                    <?= $error; ?>
+                </div>
+            <?php } ?>
+            <form method="post" class="d-flex">
+                <input type="checkbox" name="status" id="status">
+                <input type="text" name="name" id="name" placeholder="Ajouter un item" class="form-control mx-2">
+                <input type="submit" name="saveItem" class="btn btn-primary" value="Enregistrer">
+            </form>
+        <?php } ?>
     </div>
 
 </div>
--- lib/list.php
+++ lib/list.php
@@ -14,10 +14,23 @@ function getListsByUserId(PDO $pdo, int $userId):array
     return $lists;
 }
 
+function getListById(PDO $pdo, int $id):array|bool
+{
+    $query = $pdo->prepare('SELECT * FROM list WHERE id = :id');
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
+
 function saveList(PDO $pdo, string $title, int $userId, int $categoryId, int $id=null):int|bool
 {
     if ($id) {
         // UPDATE
+        $query = $pdo->prepare("UPDATE list SET title = :title, category_id = :category_id,
+                                                        user_id = :user_id
+                                WHERE id = :id");
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
     } else {
         // INSERT
         $query = $pdo->prepare("INSERT INTO list (title, category_id, user_id)
@@ -37,4 +50,26 @@ function saveList(PDO $pdo, string $title, int $userId, int $categoryId, int $id
     } else {
         return false;
     }
+}
+
+
+function saveListItem(PDO $pdo, string $name, int $listId, bool $status = false, int $id=null):bool
+{
+    if ($id) {
+        // UPDATE
+        $query = $pdo->prepare("UPDATE item SET name = :name, list_id = :list_id,
+                                                        status = :status
+                                WHERE id = :id");
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+    } else {
+        // INSERT
+        $query = $pdo->prepare("INSERT INTO item (name, list_id, status)
+                                VALUES (:name, :list_id, :status)");
+    }
+    $query->bindValue(':name', $name, PDO::PARAM_STR);
+    $query->bindValue(':list_id', $listId, PDO::PARAM_INT);
+    $query->bindValue(':status', $status, PDO::PARAM_BOOL);
+
+    return $query->execute();
+    
 }
